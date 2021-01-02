d87de7739cab2880cdf67ac60a08fe42ae0ca7cf
Matilde Volle Fiborg <54736782+matildevf@users.noreply.github.com>
2021-01-02 16:34:33 +0100

    Update 2Kjottdeig.java

--- Øving 2/2Kjottdeig.java
+++ Øving 2/2Kjottdeig.java
@@ -2,13 +2,13 @@ import static javax.swing.JOptionPane.*;
 
 class Kjottdeig{
     public static void main (String[] args) {
-        final double aPris = 35.90;
-        final double aVekt = 450;
-        final double bPris = 39.50;
-        final double bVekt = 500;
+        final double A_PRIS = 35.90;
+        final double A_VEKT = 450;
+        final double B_PRIS = 39.50;
+        final double B_VEKT = 500;
 
-        double a = aPris/aVekt;
-        double b = bPris/bVekt;
+        double a = A_PRIS/A_VEKT;
+        double b = B_PRIS/B_VEKT;
 
         if(a<b){
             showMessageDialog(null, "Det billigste kjøttdeig-merket er A.");
