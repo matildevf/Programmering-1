0185452074e1917bad7479bfd81f783ceffbf798
Matilde Volle Fiborg <54736782+matildevf@users.noreply.github.com>
2021-01-02 16:08:55 +0100

    Update 1TommerTilCm.java

--- Øving 1/1TommerTilCm.java
+++ Øving 1/1TommerTilCm.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 class TommerTilCm{
     public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Tommer");
+        System.out.print("Tommer: ");
         double tommer = in.nextDouble();
-        double centimeter = tommer*2.54;
-        System.out.printf(tommer + "tommer er ca. %1.2f centimeter.", centimeter);
+        double cm = tommer*2.54;
+        System.out.printf(tommer + " tommer er ca. %1.2f cm.", cm);
     }
 }
